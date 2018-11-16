f5d6cfbea91c3b323e106932b1191da45a0dcf35
Lambi2000 <lammers@student.chalmers.se>
2018-11-16 10:53:27 +0100

    Added JavaDoc for Saab95 and Volvo240

--- src/Saab95.java
+++ src/Saab95.java
@@ -2,22 +2,45 @@ import java.awt.*;
 
 public class Saab95 extends Car {
 
+    /**
+     *
+     */
     public boolean turboOn;
 
+    /**
+     *
+     * @param nrDoors The number of doors of the car - int
+     * @param enginePower The engine power of the car - double
+     * @param color The color of the car - Color
+     * @param modelName The model name of the car - String
+     * @param turboOn Whether or not the turbo is activated or not - boolean
+     */
 
     public Saab95(int nrDoors, double enginePower, Color color, String modelName, boolean turboOn) {
         super(nrDoors, enginePower, color, modelName);
         this.turboOn = turboOn;
     }
 
+    /**
+     * Activate turbo
+     */
+
     public void setTurboOn() {
         turboOn = true;
     }
+
+    /**
+     * Deactivate turbo
+     */
 
     public void setTurboOff() {
         turboOn = false;
     }
 
+    /**
+     * Method which returns the speedFactor
+     * @return EnginePower * 0.01 * turbo
+     */
 
     @Override
     public double speedFactor() {
--- src/Volvo240.java
+++ src/Volvo240.java
@@ -4,10 +4,22 @@ public class Volvo240 extends Car {
 
     public final static double trimFactor = 1.25;
 
+    /**
+     * Constructor for Volvo240
+     * @param nrDoors The number of doors for the car - int
+     * @param enginePower - The engine power of the car - double
+     * @param color - The color of the car - Color
+     * @param modelName - The model name of the car - String
+     */
 
     public Volvo240(int nrDoors, double enginePower, Color color, String modelName) {
         super(nrDoors, enginePower, color, modelName);
     }
+
+    /**
+     * Method which returns speedfactor of car
+     * @return enginePower * 0.01 * trimFactor
+     */
 
     @Override
     public double speedFactor() {
