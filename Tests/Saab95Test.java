f479215576c540d5d51fb71d76f10d429ced405c
Norrland97 <vikfre@student.chalmers.se>
2018-11-16 11:39:33 +0100

    Added Test file for Saab95 Methods

---
+++ Tests/Saab95Test.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import java.awt.Color;
+
+import static org.junit.Assert.*;
+
+public class Saab95Test {
+
+    Saab95 c = new Saab95(4, 200, Color.BLACK, "Saab95", true);
+
+    @Test
+    public void setTurboOn() {
+        c.setTurboOn();
+        assertEquals(2.6, c.speedFactor());
+    }
+
+    @Test
+    public void setTurboOff() {
+        c.setTurboOff();
+        assertEquals(2.0, c.speedFactor());
+    }
+
+    @Test
+    public void gas() {
+        c.startEngine();
+        c.gas(1);
+        assertEquals( 2.7,c.getCurrentSpeed());
+    }
+
+    @Test
+    public void brake() {
+        c.startEngine();
+        c.gas(1);
+        c.brake(1);
+        assertEquals( 0.1,c.getCurrentSpeed(), 0.01);
+
+    }
+}
