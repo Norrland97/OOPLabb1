dcbb1f797372796768cbb32a5d0999a39bfe69d0
Norrland97 <vikfre@student.chalmers.se>
2018-11-16 10:48:36 +0100

    Added Comments

--- src/Car.java
+++ src/Car.java
@@ -10,6 +10,13 @@ public class Car implements Movable {
     private Color color; // Color of the car
     private String modelName; // The car model name
 
+    /**
+     *The constructor of Car
+     * @param nrDoors The ammount of doors for the new car
+     * @param enginePower The power of the enging in Horsepower
+     * @param color The color of the Car
+     * @param modelName The modelname of the car
+     */
     public Car(int nrDoors, double enginePower, Color color, String modelName) {
         this.nrDoors = nrDoors;
         this.enginePower = enginePower;
@@ -23,57 +30,102 @@ public class Car implements Movable {
     //----------------Getters and setters----------------
 
 
+    /**
+     * @return Returns the position of the car
+     */
     public Point getCurrentPos() {
         return currentPos;
     }
 
+    /**
+     * sets the position of Car
+     * @param currentPos
+     */
     public void setCurrentPos(Point currentPos) {
         this.currentPos = currentPos;
     }
 
+    /**
+     * @return Returns current direction of Car
+     */
     public double getCurrentDirection() {
         return currentDirection;
     }
 
+    /**
+     *sets the direction of Car
+     * @param currentDirection
+     */
     public void setCurrentDirection(double currentDirection) {
         this.currentDirection = currentDirection;
     }
 
 
+    /**
+     * @return Returns the ammount of doors on the Car
+     */
     public int getNrDoors() {
         return nrDoors;
     }
 
-    protected double getEnginePower() {
+    /**
+     * @return Returns the power of the engine of the car
+     */
+    public double getEnginePower() {
         return enginePower;
     }
 
+    /**
+     * @return Returns the speed of the car
+     */
     public double getCurrentSpeed() {
         return currentSpeed;
     }
 
+    /**
+     * @return returns the color of the car
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * changes the color of the car
+     * @param clr
+     */
     public void setColor(Color clr) {
         color = clr;
     }
 
+    /**
+     * "Starts up the engine"
+     * Actually: gives the car an small ammount of speed
+     */
     public void startEngine() {
         currentSpeed = 0.1;
     }
 
+    /**
+     * Sets the speed of the car to 0, The car stops;
+     */
     public void stopEngine() {
         currentSpeed = 0;
     }
 
+    /**
+     * An internal method to get the speed factor of the car
+     * @return speedFactor, the base speedfactor of Car
+     */
     protected double speedFactor() {
         return enginePower * 0.1;
     }
 
     //--------Change speed----------
 
+    /**
+     * A method used to increase the speed of the car based of the cars speedFactor and the ammount given to increase it
+     * @param amount A value between 0 and 1 given to increase the speed of the car;
+     */
     protected void incrementSpeed(double amount) {
         currentSpeed = getCurrentSpeed() + speedFactor() * amount;
     }
@@ -104,13 +156,17 @@ public class Car implements Movable {
     }
 
 
-
-
+    /**
+     * turns the car to the left by increasing currentDirection
+     */
     @Override
     public void turnLeft() {
         currentDirection++;
     }
 
+    /**
+     *  turns the car to the right by decreasing currentDirection
+     */
     @Override
     public void turnRight() {
         currentDirection--;
