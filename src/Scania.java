fca6f3c8bfd854e4e8726532338466a73895ee9a
Norrland97 <vikfre@student.chalmers.se>
2018-11-21 10:19:18 +0100

    Wrote some in Scania

--- src/Scania.java
+++ src/Scania.java
@@ -1,3 +1,25 @@
-public class Scania {
+import java.awt.*;
+
+public class Scania extends Car {
+
+    private final double flatBedAngle;
+
+    /**
+     * The constructor of Scania
+     *  @param nrDoors     The ammount of doors for the new car
+     * @param enginePower The power of the enging in Horsepower
+     * @param color       The color of the Car
+     * @param modelName   The modelname of the car
+     * @param flatBedAngle  The current angle of the flatbed
+     */
+    public Scania(int nrDoors, double enginePower, Color color, String modelName, double flatBedAngle) {
+        super(nrDoors, enginePower, color, modelName);
+        this.flatBedAngle = flatBedAngle;
+    }
+
+    public double getFlatBedAngle() {
+        return flatBedAngle;
+    }
+
     
 }
