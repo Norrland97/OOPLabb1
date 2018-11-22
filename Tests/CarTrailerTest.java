8e9f1aa303edc551f543c298c76aee5372ad7afa
Norrland97 <vikfre@student.chalmers.se>
2018-11-22 09:26:46 +0100

    small fixes

--- Tests/CarTrailerTest.java
+++ Tests/CarTrailerTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class CarTrailerTest {
 
-    CarTrailer ct = new CarTrailer(2, 200, Color.BLACK, "Cartrailer");
+    CarTrailer ct = new CarTrailer(2, 200, Color.BLACK, "Cartrailer", 5);
 
     @Test
     public void loadCar() {
--- src/CarTrailer.java
+++ src/CarTrailer.java
@@ -9,26 +9,28 @@ import java.util.Stack;
 public class CarTrailer extends Car {
 
     private Boolean flatBedDown;
-    private final int maxLoad = 5;
+    private final int maxLoad;
     private Stack<Car> loadedCars = new Stack<>();
 
     /**
      * Constructor of a CarTrailer
-     * @param nrDoors The number of doors of the CarTrailer
+     *
+     * @param nrDoors     The number of doors of the CarTrailer
      * @param enginePower The engine power of the CarTrailer
-     * @param color The color of the CarTrailer
-     * @param modelName The model name of the CarTrailer
+     * @param color       The color of the CarTrailer
+     * @param modelName   The model name of the CarTrailer
      */
-    public CarTrailer(int nrDoors, double enginePower, Color color, String modelName){
+    public CarTrailer(int nrDoors, double enginePower, Color color, String modelName, int maxLoad) {
         super(nrDoors, enginePower, color, modelName);
         this.flatBedDown = false;
+        this.maxLoad = maxLoad;
     }
 
     /**
      * Method which lowers the flat bed. Allows for loading of cars.
      */
-    public void lowerFlatBed(){
-        if(getCurrentSpeed() == 0)
+    public void lowerFlatBed() {
+        if (getCurrentSpeed() == 0)
             flatBedDown = true;
         else
             System.out.println("Cant lower flat bed while driving");
@@ -37,21 +39,22 @@ public class CarTrailer extends Car {
     /**
      * Method which raises the flat bed. Prevents the options of loading of cars.
      */
-    public void raiseFlatBed(){
+    public void raiseFlatBed() {
         flatBedDown = false;
     }
 
     /**
      * Method which loads a Car onto the CarTrailer. The amount of cars loaded is decided by the variable maxLoad.
      * The Car needs to be in the proximity of the CarTrailer
+     *
      * @param car The car which will get loaded.
      */
-    public void loadCar(Car car){
+    public void loadCar(Car car) {
 
-        if (flatBedDown && loadedCars.size() <= maxLoad /* && position of car is close to carTrailer*/){
+        if (flatBedDown && loadedCars.size() <= maxLoad /* && position of car is close to carTrailer*/) {
             loadedCars.push(car);
             //Update position of car to the position of CarTrailer
-        }else{
+        } else {
             System.out.println("Can only load car when flat bed is down.");
         }
     }
@@ -60,14 +63,14 @@ public class CarTrailer extends Car {
      * Method which unloads the Car that's been loaded most recently.
      * The Car will be unloaded in the CarTrailers proximity.
      */
-    public void unloadCar(){
+    public void unloadCar() {
 
         Car car = loadedCars.peek();
 
-        if (flatBedDown && loadedCars.size() > 0){
+        if (flatBedDown && loadedCars.size() > 0) {
             loadedCars.pop();
             //Update position of car to the position of CarTrailer
-        }else{
+        } else {
             System.out.println("Can only load car when flat bed is down.");
         }
     }
