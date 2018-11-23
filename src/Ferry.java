6d01f1ac4716b10fed7defad27ce8ad90abc5f18
Lambi2000 <lammers@student.chalmers.se>
2018-11-23 11:19:36 +0100

    Edited Ferry

--- src/Ferry.java
+++ src/Ferry.java
@@ -8,10 +8,8 @@ import java.util.List;
 
 public class Ferry extends Vehicle {
 
-    private boolean isDocked; // Whether the ferry is docked or not, allows for loading of cars.
-    private int maxLoad;
-    private List<Car> loadedCars;
-    private double loadingProximity = 10;
+    //private boolean isDocked; // Whether the ferry is docked or not, allows for loading of cars.
+    private Transporter<Car> parent;
 
     /**
      * The constructor of Ferry
@@ -20,35 +18,23 @@ public class Ferry extends Vehicle {
      * @param modelName The model name of the Ferry
      * @param isDocked Whether the ferry is docked or not. Allows for loading of Cars
      * @param maxLoad The maximum number of Cars which the Ferry can load
-     * @param loadedCars A list of the Cars which has been loaded
-     * @param loadingProximity The distance from which the Car needs to be to be loaded onto the Ferry
      */
     public Ferry(double enginePower, Color color, String modelName, boolean isDocked,
-                 int maxLoad, List<Car> loadedCars, double loadingProximity) {
+                 int maxLoad) {
         super(enginePower, color, modelName);
-        this.isDocked = isDocked;
-        this.maxLoad = maxLoad;
-        this.loadedCars = loadedCars;
-        this.loadingProximity = loadingProximity;
+        this.parent = new Transporter<>(maxLoad, isDocked, getCurrentPos());
+        //this.isDocked = isDocked;
     }
 
     public void loadCar(Car car) {
-
-        if (isDocked && parent.checkProximity(car.getCurrentPos())) {
-            loadedCars.add(car);
-            parent.moveLoadedCars();
-        } else if(loadedCars.size() >= maxLoad) {
-            System.out.println("Ferry is full");
-        }else if(!isDocked){
-            System.out.println("Can only load car when ferry is docked.");
-        }
+        parent.loadCar(car);
     }
 
     public void unloadCar() {
 
         Car car = loadedCars.get(0);
 
-        if (isDocked && loadedCars.size() > 0) {
+        if (parent.isCanLoad() && loadedCars.size() > 0) {
             loadedCars.remove(0);
             car.getCurrentPos().x = parent.getCurrentPos().x - 5;
             car.getCurrentPos().y = parent.getCurrentPos().y - 5;
